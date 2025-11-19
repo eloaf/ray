9e0f1b7c2de6866aa54bbc1ab67ac33c3b708d92
harshit <harshit@anyscale.com>
2025-11-19 14:36:32 +0000

    improve notebook
    Signed-off-by: harshit <harshit@anyscale.com>

--- doc/source/serve/tutorials/asynchronous-inference/ci/tests.sh
+++ doc/source/serve/tutorials/asynchronous-inference/ci/tests.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-python content/server.py server.py
-serve run server:app
-
 # Example: convert and run a notebook
-python ci/nb2py.py content/asynchronous-inference.ipynb /tmp/asynchronous-inference.py --ignore-cmds
+python ci/nb2py.py content/asynchronous-inference.ipynb /tmp/asynchronous-inference.py
 python /tmp/asynchronous-inference.py
 
 python content/client.py
 
-rm server.py
 rm /tmp/asynchronous-inference.py
